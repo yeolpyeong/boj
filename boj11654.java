d702c19bcf38bdfe9820786140710b997a0cd223
김평열 <yeolpyeong@gmail.com>
2017-11-21 23:42:32 +0900

    아스키 코드

---
+++ boj11654.java
@@ -0,0 +1,16 @@
+/*
+ * 아스키 코드
+ * https://www.acmicpc.net/problem/11654
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		System.out.println((int)s.charAt(0));
+	}
+}
