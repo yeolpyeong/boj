73180b627d69327a22cc2868b9eafd97bdbe93e7
김평열 <yeolpyeong@gmail.com>
2017-12-12 00:07:41 +0900

    Hello World

---
+++ boj2557.java
@@ -0,0 +1,12 @@
+/*
+ * Hello World
+ * https://www.acmicpc.net/problem/2557
+ */
+
+package boj;
+
+public class boj2557 {
+	public static void main(String[] args) {
+		System.out.print("Hello World!");
+	}
+}
