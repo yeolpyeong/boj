d9fc05d873e5f9e7eb385fec4d614616ab20aaf5
김평열 <yeolpyeong@gmail.com>
2017-12-12 00:10:14 +0900

    A+B

---
+++ boj1000.java
@@ -0,0 +1,17 @@
+/*
+ * A+B
+ * https://www.acmicpc.net/problem/1000
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj1000 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.print(A + B);
+	}
+}
