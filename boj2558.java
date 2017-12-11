7281614c6379bae54fa4339e81fec6f351d6d778
김평열 <yeolpyeong@gmail.com>
2017-12-12 00:12:54 +0900

    A+B - 2

---
+++ boj2558.java
@@ -0,0 +1,17 @@
+/*
+ * A+B - 2
+ * https://www.acmicpc.net/problem/2558
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj2558 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.print(A + B);
+	}
+}
