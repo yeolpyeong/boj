705dedd87ae8807e38d438dbdf031e092d37b8e8
김평열 <yeolpyeong@gmail.com>
2017-12-13 23:57:34 +0900

    개

---
+++ boj10172.java
@@ -0,0 +1,12 @@
+/*
+ * °³
+ * https://www.acmicpc.net/problem/10172
+ */
+
+package boj;
+
+public class boj10172 {
+	public static void main(String[] args) {
+		System.out.print("|\\_/|\n" + "|q p|   /}\n" + "( 0 )\"\"\"\\\n" + "|\"^\"`    |\n" + "||_/=\\\\__|");
+	}
+}
