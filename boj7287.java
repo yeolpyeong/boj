6a82f37062cce704857236784d617a19325fc65a
김평열 <yeolpyeong@gmail.com>
2017-12-13 23:41:46 +0900

    등록

---
+++ boj7287.java
@@ -0,0 +1,12 @@
+/*
+ * µî·Ï
+ * https://www.acmicpc.net/problem/7287
+ */
+
+package boj;
+
+public class boj7287 {
+	public static void main(String[] args) {
+		System.out.print("66\nyeolpyeong");
+	}
+}
