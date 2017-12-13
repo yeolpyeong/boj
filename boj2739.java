3a2c6c9ca1162948ebeaf38103085d7b091ec68e
김평열 <yeolpyeong@gmail.com>
2017-12-13 14:28:23 +0900

    구구단

---
+++ boj2739.java
@@ -0,0 +1,18 @@
+/*
+ * ±¸±¸´Ü 
+ * https://www.acmicpc.net/problem/2739
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj2739 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(N + " * " + i + " = " + N * i);
+		}
+	}
+}
