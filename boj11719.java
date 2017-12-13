7c3274aeb8970f495674d51b48d86d046b8ee07a
김평열 <yeolpyeong@gmail.com>
2017-12-13 13:45:22 +0900

    그대로 출력하기 2

---
+++ boj11719.java
@@ -0,0 +1,17 @@
+/*
+ * 그대로 출력하기 2
+ * https://www.acmicpc.net/problem/11719
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj11719 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			System.out.println(sc.nextLine());
+		}
+	}
+}
