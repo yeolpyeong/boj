7b3b9c23eacd3b818f093526ae94f35be810e97c
김평열 <yeolpyeong@gmail.com>
2017-12-13 14:20:45 +0900

    N 찍기

---
+++ boj2741.java
@@ -0,0 +1,18 @@
+/*
+ * N Âï±â 
+ * https://www.acmicpc.net/problem/2741
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj2741 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i = 1; i <= N; i++) {
+			System.out.println(i);
+		}
+	}
+}
