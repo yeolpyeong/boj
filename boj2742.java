5bf563aaaa484e8e7fe4e5f5e57d1cf086a8e494
김평열 <yeolpyeong@gmail.com>
2017-12-13 14:24:24 +0900

    기찍 N

---
+++ boj2742.java
@@ -0,0 +1,18 @@
+/*
+ * ±âÂï N 
+ * https://www.acmicpc.net/problem/2742
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj2742 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		while (N > 0) {
+			System.out.println(N--);
+		}
+	}
+}
