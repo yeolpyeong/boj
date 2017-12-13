d3455d85760f7a9404bdb6f6796da4cb39a113fd
김평열 <yeolpyeong@gmail.com>
2017-12-13 18:05:11 +0900

    별찍기 - 7

---
+++ boj2444.java
@@ -0,0 +1,34 @@
+/*
+ * º°Âï±â - 7
+ * https://www.acmicpc.net/problem/2444
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj2444 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int i = 1;
+		boolean flag = false;
+		while (i >= 1) {
+			for (int j = 1; j <= N - i; j++) {
+				System.out.print(' ');
+			}
+			for (int j = 1; j <= 2 * i - 1; j++) {
+				System.out.print('*');
+			}
+			System.out.println();
+
+			if (i < N && flag == false) {
+				i++;
+				if (i >= N)
+					flag = true;
+			} else {
+				i--;
+			}
+		}
+	}
+}
