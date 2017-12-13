ddf68b4c6637476089f252dec1be51c83a3330db
김평열 <yeolpyeong@gmail.com>
2017-12-13 23:45:24 +0900

    We love kriii

---
+++ boj10718.java
@@ -0,0 +1,12 @@
+/*
+ * We love kriii
+ * https://www.acmicpc.net/problem/10718
+ */
+
+package boj;
+
+public class boj10718 {
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군\n강한친구 대한육군");
+	}
+}
