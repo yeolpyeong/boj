a0be590ffd608e14e5d2b449f06219774985c497
김평열 <yeolpyeong@gmail.com>
2017-12-13 23:23:14 +0900

    A-B

---
+++ boj1001.java
@@ -0,0 +1,17 @@
+/*
+ * A-B
+ * https://www.acmicpc.net/problem/1001
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj1001 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.print(A - B);
+	}
+}
