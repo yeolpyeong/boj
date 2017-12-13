2f14c08f5062d62cfd1fcad9635e4af88e131ed3
김평열 <yeolpyeong@gmail.com>
2017-12-13 14:51:08 +0900

    별찍기 - 1

---
+++ boj2438.java
@@ -0,0 +1,19 @@
+/*
+ * º°Âï±â - 1
+ * https://www.acmicpc.net/problem/2438
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj2438 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		String stars = "";
+		for (int i = 1; i <= N; i++) {
+			System.out.println(stars += '*');
+		}
+	}
+}
