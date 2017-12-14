fee7cc9809a103c56183c87a502d23fb57c7888b
김평열 <yeolpyeong@gmail.com>
2017-12-14 14:27:39 +0900

    A*B

---
+++ boj10998.java
@@ -0,0 +1,17 @@
+/*
+ * A*B
+ * https://www.acmicpc.net/problem/10998
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj10998 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.print(A * B);
+	}
+}
