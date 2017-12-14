0f2039d176ddcccaaded09c9b8ef2983d140fa20
김평열 <yeolpyeong@gmail.com>
2017-12-14 15:26:16 +0900

    A/B

---
+++ boj1008.java
@@ -0,0 +1,17 @@
+/*
+ * A/B
+ * https://www.acmicpc.net/problem/1008
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj1008 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double A = sc.nextDouble();
+		int B = sc.nextInt();
+		System.out.print(A / B);
+	}
+}
