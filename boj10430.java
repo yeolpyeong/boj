fe85b1ba5e217ec00ecfc982d249483d3913836c
김평열 <yeolpyeong@gmail.com>
2017-12-14 15:37:20 +0900

    나머지

---
+++ boj10430.java
@@ -0,0 +1,21 @@
+/*
+ * ³ª¸ÓÁö
+ * https://www.acmicpc.net/problem/10430 
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj10430 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		System.out.println((A + B) % C);
+		System.out.println((A % C + B % C) % C);
+		System.out.println((A * B) % C);
+		System.out.println((A % C * B % C) % C);
+	}
+}
