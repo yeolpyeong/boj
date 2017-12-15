2fc224522074bf7fb6620d76d1e71e23f39fddd2
김평열 <yeolpyeong@gmail.com>
2017-12-15 15:48:15 +0900

    평균 점수

---
+++ boj10039.java
@@ -0,0 +1,20 @@
+/*
+ * Æò±Õ Á¡¼ö
+ * https://www.acmicpc.net/problem/10039
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj10039 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+		for (int i = 1; i <= 5; i++) {
+			int score = sc.nextInt();
+			sum += score < 40 ? 40 : score;
+		}
+		System.out.println(sum / 5);
+	}
+}
