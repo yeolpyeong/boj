71ed639216255d876ad7b7f12ee0dcc4554f8ca3
김평열 <yeolpyeong@gmail.com>
2017-12-15 12:04:22 +0900

    별 찍기 - 17

---
+++ boj10992.java
@@ -0,0 +1,21 @@
+/*
+ * º° Âï±â - 17
+ * https://www.acmicpc.net/problem/10992
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj10992 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N + i - 1; j++) {
+				System.out.print(j == N - i + 1 || j == N + i - 1 || i == N ? '*' : ' ');
+			}
+			System.out.println();
+		}
+	}
+}
