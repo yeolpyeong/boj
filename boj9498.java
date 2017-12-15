1ecba72585d99c0579798ce3be82572df8bcbb18
김평열 <yeolpyeong@gmail.com>
2017-12-15 14:08:49 +0900

    시험 성적

---
+++ boj9498.java
@@ -0,0 +1,16 @@
+/*
+ * 시험 성적
+ * https://www.acmicpc.net/problem/9498
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj9498 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int score = sc.nextInt();
+		System.out.print("FFFFFFDCBAA".charAt(score / 10));
+	}
+}
