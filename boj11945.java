1b76fc2eeb931587a0224399bbefca83b1b1def3
김평열 <yeolpyeong@gmail.com>
2017-12-18 02:11:24 +0900

    뜨거운 붕어빵

---
+++ boj11945.java
@@ -0,0 +1,19 @@
+/*
+ * ¶ß°Å¿î ºØ¾î»§
+ * https://www.acmicpc.net/problem/11945
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj11945 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		while (sc.hasNext()) {
+			System.out.println(new StringBuilder(sc.next()).reverse());
+		}
+	}
+}
