d48a8fb39dfc1fc0aead7f8def9af34f276b8950
김평열 <yeolpyeong@gmail.com>
2017-12-18 00:24:20 +0900

    고려대는 사랑입니다

---
+++ boj11942.java
@@ -0,0 +1,12 @@
+/*
+ * 고려대는 사랑입니다.
+ * https://www.acmicpc.net/problem/11942
+ */
+
+package boj;
+
+public class boj11942 {
+	public static void main(String[] args) {
+		System.out.print("고려대학교");
+	}
+}
