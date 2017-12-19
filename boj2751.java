ea234da4c3d87feefed1f4e216e698eae741995e
김평열 <yeolpyeong@gmail.com>
2017-12-19 23:00:21 +0900

    수 정렬하기 2

---
+++ boj2751.java
@@ -0,0 +1,24 @@
+/*
+ * 수 정렬하기 2
+ * https://www.acmicpc.net/problem/2751
+ */
+
+package boj;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class boj2751 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] array = new int[N];
+		for (int i = 0; i < N; i++) {
+			array[i] = sc.nextInt();
+		}
+		Arrays.sort(array);
+		for (int i = 0; i < N; i++) {
+			System.out.println(array[i]);
+		}
+	}
+}
