803759b7ffd614088f954c9c8a2986d52a97753b
김평열 <yeolpyeong@gmail.com>
2017-12-22 00:50:57 +0900

    R2

---
+++ boj3046.java
@@ -0,0 +1,17 @@
+/*
+ * R2
+ * https://www.acmicpc.net/problem/3046
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj3046 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int R1 = sc.nextInt();
+		int S = sc.nextInt();
+		System.out.print(2 * S - R1);
+	}
+}
