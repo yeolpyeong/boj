55c90b40da37d5decc05c52a21f6e4b5b4f26366
김평열 <yeolpyeong@gmail.com>
2017-12-22 00:10:50 +0900

    일곱 난쟁이

---
+++ boj2309.java
@@ -0,0 +1,34 @@
+/*
+ * ¿œ∞ˆ ≥≠¿Ô¿Ã
+ * https://www.acmicpc.net/problem/2309
+ */
+
+package boj;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class boj2309 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] numbers = new int[9];
+		int sum = 0;
+		for (int i = 0; i < 9; i++) {
+			numbers[i] = sc.nextInt();
+			sum += numbers[i];
+		}
+		Arrays.sort(numbers);
+		sum -= 100;
+		for (int i = 0; i < 8; i++) {
+			for (int j = i + 1; j < 9; j++) {
+				if (numbers[i] + numbers[j] == sum) {
+					for (int k = 0; k < 9; k++) {
+						if (k != i && k != j)
+							System.out.println(numbers[k]);
+					}
+					return;
+				}
+			}
+		}
+	}
+}
