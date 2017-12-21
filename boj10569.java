cb4d90e5800ca62b51cd4303d12bd5a1924751cb
김평열 <yeolpyeong@gmail.com>
2017-12-22 00:57:10 +0900

    다면체

---
+++ boj10569.java
@@ -0,0 +1,20 @@
+/*
+ * ´Ù¸éÃ¼
+ * https://www.acmicpc.net/problem/10569
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj10569 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while (T-- > 0) {
+			int V = sc.nextInt();
+			int E = sc.nextInt();
+			System.out.println(2 - V + E);
+		}
+	}
+}
