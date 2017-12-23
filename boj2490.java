1da5d9c5c806aaa46cb30605879775d712ce16ce
김평열 <yeolpyeong@gmail.com>
2017-12-23 09:42:15 +0900

    윷놀이

---
+++ boj2490.java
@@ -0,0 +1,22 @@
+/*
+ * ¿∑≥Ó¿Ã
+ * https://www.acmicpc.net/problem/2490
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj2490 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		for (int i = 1; i <= 3; i++) {
+			int sum = 0;
+			for (int j = 1; j <= 4; j++) {
+				sum += sc.nextInt();
+			}
+			String yut = "DCBAE";
+			System.out.println(yut.charAt(sum));
+		}
+	}
+}
