952fcd45d71b256839e000600a604ddcf11f09af
김평열 <yeolpyeong@gmail.com>
2017-12-23 09:23:15 +0900

    10부제

---
+++ boj10797.java
@@ -0,0 +1,22 @@
+/*
+ * 10∫Œ¡¶
+ * https://www.acmicpc.net/problem/10797
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj10797 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int d = sc.nextInt();
+		int c = 0;
+		for (int i = 1; i <= 5; i++) {
+			int n = sc.nextInt();
+			if (d == n)
+				c++;
+		}
+		System.out.print(c);
+	}
+}
