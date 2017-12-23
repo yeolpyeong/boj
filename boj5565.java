d9d75b7b09345e1eb06717a1846bc145092a77c5
김평열 <yeolpyeong@gmail.com>
2017-12-23 15:22:21 +0900

    영수증

---
+++ boj5565.java
@@ -0,0 +1,19 @@
+/*
+ * ¿µ¼öÁõ
+ * https://www.acmicpc.net/problem/5565
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj5565 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int price = sc.nextInt();
+		for (int i = 1; i <= 9; i++) {
+			price -= sc.nextInt();
+		}
+		System.out.print(price);
+	}
+}
