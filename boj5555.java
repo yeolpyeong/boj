090852fa9bd1d7f3589723f7a5b1f2b6ed7e42d7
김평열 <yeolpyeong@gmail.com>
2017-12-23 13:32:31 +0900

    반지

---
+++ boj5555.java
@@ -0,0 +1,24 @@
+/*
+ * ¹ÝÁö
+ * https://www.acmicpc.net/problem/5555
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj5555 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String word = sc.next();
+		int N = sc.nextInt();
+		int count = 0;
+		while (N-- > 0) {
+			String ring = sc.next();
+			ring += ring.substring(0, word.length() - 1);
+			if (ring.contains(word))
+				count++;
+		}
+		System.out.print(count);
+	}
+}
