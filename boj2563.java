ad018a6c471a75a17c89d5ee2e286a1d6d3e6002
김평열 <yeolpyeong@gmail.com>
2017-12-24 16:57:33 +0900

    색종이

---
+++ boj2563.java
@@ -0,0 +1,28 @@
+/*
+ * »öÁ¾ÀÌ
+ * https://www.acmicpc.net/problem/2563
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj2563 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] drawingPaper = new int[100][100];
+		int area = 0;
+		while (n-- > 0) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			for (int i = x; i < x + 10; i++) {
+				for (int j = y; j < y + 10; j++) {
+					if (drawingPaper[i][j] == 0)
+						drawingPaper[i][j] = ++area;
+				}
+			}
+		}
+		System.out.print(area);
+	}
+}
