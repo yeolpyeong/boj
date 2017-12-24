65aff11a2d99e4a774bee6b83542d7057434472f
김평열 <yeolpyeong@gmail.com>
2017-12-24 14:05:10 +0900

    에라토스테네스의 체

--- boj2960.java
+++ boj2960.java
@@ -12,32 +12,23 @@ public class boj2960 {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int K = sc.nextInt();
+		System.out.print(boj2960(N, K));
+	}
+
+	public static int boj2960(int N, int K) {
 		int[] numbers = new int[N + 1];
 		int k = 0;
 		for (int i = 2; i <= N; i++) {
-			if (isPrime(i)) {
-				for (int j = 1; i * j <= N; j++) {
-					if (numbers[i * j] == 0) {
-						k++;
-						numbers[i * j] = 1;
-						if (k == K) {
-							System.out.print(i * j);
-							return;
-						}
+			for (int j = 1; i * j <= N; j++) {
+				if (numbers[i * j] == 0) {
+					k++;
+					numbers[i * j] = 1;
+					if (k == K) {
+						return i * j;
 					}
 				}
 			}
 		}
-	}
-
-	public static boolean isPrime(int n) {
-		if (n == 1)
-			return false;
-
-		for (int i = 2; i <= Math.sqrt(n); i++) {
-			if (n % i == 0)
-				return false;
-		}
-		return true;
+		return 0;
 	}
 }
