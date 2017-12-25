536f872a086ca73537b278758697aa23c1830228
김평열 <yeolpyeong@gmail.com>
2017-12-25 20:29:31 +0900

    직각삼각형

---
+++ boj4153.java
@@ -0,0 +1,27 @@
+/*
+ * Á÷°¢»ï°¢Çü
+ * https://www.acmicpc.net/problem/4153
+ */
+
+package boj;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class boj4153 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] length = new int[3];
+		while (true) {
+			length[0] = sc.nextInt();
+			length[1] = sc.nextInt();
+			length[2] = sc.nextInt();
+			if (length[0] + length[1] + length[2] == 0)
+				break;
+
+			Arrays.sort(length);
+			System.out.println(
+					length[0] * length[0] + length[1] * length[1] == length[2] * length[2] ? "right" : "wrong");
+		}
+	}
+}
