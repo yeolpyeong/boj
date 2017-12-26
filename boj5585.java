a3e66682795159d0f930f165447d8e0e3b818516
김평열 <yeolpyeong@gmail.com>
2017-12-26 10:03:00 +0900

    거스름돈

---
+++ boj5585.java
@@ -0,0 +1,21 @@
+/*
+ * °Å½º¸§µ·
+ * https://www.acmicpc.net/problem/5585
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj5585 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int change = 1000 - sc.nextInt();
+		int n = 0;
+		while (change > 0) {
+			n += change % 10 / 5 + change % 5;
+			change /= 10;
+		}
+		System.out.print(n);
+	}
+}
