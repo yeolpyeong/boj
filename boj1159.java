a56f233ab9e999aa1334e2bcc349bf6ff32141f8
김평열 <yeolpyeong@gmail.com>
2017-12-26 12:41:42 +0900

    농구 경기

--- boj1159.java
+++ boj1159.java
@@ -1,31 +1,25 @@
 /*
- * ≥Û±∏ ∞Ê±‚
+ * ÎÜçÍµ¨ Í≤ΩÍ∏∞
  * https://www.acmicpc.net/problem/1159
  */
 
 package boj;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class boj1159 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		String[] list = new String[N];
-		for (int i = 0; i < N; i++) {
-			list[i] = sc.next();
-		}
-		Arrays.sort(list);
 		int[] alphabet = new int[26];
 		for (int i = 0; i < N; i++) {
-			alphabet[(int) list[i].charAt(0) - 'a']++;
+			String name = sc.next();
+			alphabet[(int) name.charAt(0) - 'a']++;
 		}
-		String letter = "abcdefghijklmnopqrstuvwxyz";
 		String res = "";
-		for (int i = 0; i < 26; i++) {
-			if (alphabet[i] >= 5)
-				res += letter.charAt(i);
+		for (char c = 'a'; c <= 'z'; c++) {
+			if (alphabet[c - 'a'] >= 5)
+				res += c;
 		}
 		System.out.print(res.length() > 0 ? res : "PREDAJA");
 	}
