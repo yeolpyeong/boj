82f58c1cee12b04d3bde1cf2cd0ef6381fb37406
김평열 <yeolpyeong@gmail.com>
2017-12-27 16:58:39 +0900

    유니크

---
+++ boj5533.java
@@ -0,0 +1,38 @@
+/*
+ * ¿Ø¥œ≈©
+ * https://www.acmicpc.net/problem/5533
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj5533 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[][] score = new int[N][3];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < 3; j++) {
+				score[i][j] = sc.nextInt();
+			}
+		}
+
+		int[] sum = new int[N];
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < N; j++) {
+				boolean flag = true;
+				for (int k = 0; k < N; k++) {
+					if (j != k && score[j][i] == score[k][i])
+						flag = false;
+				}
+				if (flag == true)
+					sum[j] += score[j][i];
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			System.out.println(sum[i]);
+		}
+	}
+}
