7989d004ab6e006ef2c1ba6de646a5994995d8dc
김평열 <yeolpyeong@gmail.com>
2017-12-27 12:59:45 +0900

    방학 숙제

---
+++ boj5532.java
@@ -0,0 +1,20 @@
+/*
+ * ¹æÇÐ ¼÷Á¦
+ * https://www.acmicpc.net/problem/5532
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj5532 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int L = sc.nextInt();
+		double A = sc.nextInt();
+		double B = sc.nextInt();
+		int C = sc.nextInt();
+		int D = sc.nextInt();
+		System.out.print(L - (int) Math.ceil(Math.max(A / C, B / D)));
+	}
+}
