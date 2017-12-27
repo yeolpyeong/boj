23fbccc63be138ba8d79cf03b1ab954b4c35f047
김평열 <yeolpyeong@gmail.com>
2017-12-27 12:44:45 +0900

    소음

---
+++ boj2935.java
@@ -0,0 +1,19 @@
+/*
+ * º“¿Ω
+ * https://www.acmicpc.net/problem/2935
+ */
+
+package boj;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class boj2935 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger A = sc.nextBigInteger();
+		String O = sc.next();
+		BigInteger B = sc.nextBigInteger();
+		System.out.print(O.equals("+") ? A.add(B) : A.multiply(B));
+	}
+}
