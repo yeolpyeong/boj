5df805167f61d542120ea890e4c514836a001a01
김평열 <yeolpyeong@gmail.com>
2017-12-28 16:34:13 +0900

    콜센터

---
+++ boj5339.java
@@ -0,0 +1,13 @@
+/*
+ * ÄÝ¼¾ÅÍ
+ * https://www.acmicpc.net/problem/5339
+ */
+
+package boj;
+
+public class boj5339 {
+	public static void main(String[] args) {
+		System.out.print("     /~\\\n" + "    ( oo|\n" + "    _\\=/_\n" + "   /  _  \\\n" + "  //|/.\\|\\\\ \n"
+				+ " ||  \\ /  ||\n" + "============\n" + "|          |\n" + "|          |\n" + "|          |");
+	}
+}
