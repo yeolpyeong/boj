b83187f7c11716cb2de233a3001158b6d297dd10
김평열 <yeolpyeong@gmail.com>
2017-12-28 16:31:56 +0900

    마이크로소프트 로고

---
+++ boj5338.java
@@ -0,0 +1,13 @@
+/*
+ * 마이크로소프트 로고
+ * https://www.acmicpc.net/problem/5338
+ */
+
+package boj;
+
+public class boj5338 {
+	public static void main(String[] args) {
+		System.out.print("       _.-;;-._\n" + "'-..-'|   ||   |\n" + "'-..-'|_.-;;-._|\n" + "'-..-'|   ||   |\n"
+				+ "'-..-'|_.-''-._|");
+	}
+}
