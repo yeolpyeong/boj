c36e8b3df66a2520462f1c81481b5c0307427cc2
김평열 <yeolpyeong@gmail.com>
2017-12-28 17:37:19 +0900

    더하기

---
+++ boj14918.java
@@ -0,0 +1,17 @@
+/*
+ * ¥ı«œ±‚
+ * https://www.acmicpc.net/problem/14918
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj14918 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.print(a + b);
+	}
+}
