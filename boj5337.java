ee377c686db3936cb89024a99672cb74528b6dbd
김평열 <yeolpyeong@gmail.com>
2017-12-28 16:28:47 +0900

    웰컴

---
+++ boj5337.java
@@ -0,0 +1,12 @@
+/*
+ * À£ÄÄ
+ * https://www.acmicpc.net/problem/5337
+ */
+
+package boj;
+
+public class boj5337 {
+	public static void main(String[] args) {
+		System.out.print(".  .   .\n" + "|  | _ | _. _ ._ _  _\n" + "|/\\|(/.|(_.(_)[ | )(/.");
+	}
+}
