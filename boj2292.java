ab1808617a0c84826ecad90bc69f3a12c9420be9
김평열 <yeolpyeong@gmail.com>
2018-01-06 12:34:12 +0900

    벌집

---
+++ boj2292.java
@@ -0,0 +1,21 @@
+/*
+ * ¹úÁý
+ * https://www.acmicpc.net/problem/2292
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj2292 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int C = 1;
+		while (N > 1) {
+			N -= 6 * C;
+			C++;
+		}
+		System.out.print(C);
+	}
+}
