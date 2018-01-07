1f01e710e69ea3f187db48b79ff563f21267c6f7
김평열 <yeolpyeong@gmail.com>
2018-01-07 19:02:29 +0900

    괄호

---
+++ boj9012.java
@@ -0,0 +1,22 @@
+/*
+ * °ýÈ£
+ * https://www.acmicpc.net/problem/9012
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj9012 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while (T-- > 0) {
+			String PS = sc.next();
+			while (PS.contains("()")) {
+				PS = PS.replace("()", "");
+			}
+			System.out.println(PS.isEmpty() ? "YES" : "NO");
+		}
+	}
+}
