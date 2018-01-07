db1bcdc694ee4e65f06f584720ad0e3efce60739
김평열 <yeolpyeong@gmail.com>
2018-01-07 19:15:19 +0900

    더하기

---
+++ boj9085.java
@@ -0,0 +1,23 @@
+/*
+ * ´õÇÏ±â
+ * https://www.acmicpc.net/problem/9085
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj9085 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while (T-- > 0) {
+			int N = sc.nextInt();
+			int sum = 0;
+			while (N-- > 0) {
+				sum += sc.nextInt();
+			}
+			System.out.println(sum);
+		}
+	}
+}
