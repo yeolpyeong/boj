99b9b82d86748189aeb4330105e18022756e3c6e
김평열 <yeolpyeong@gmail.com>
2018-01-07 20:01:30 +0900

    알파벳 개수

---
+++ boj10808.java
@@ -0,0 +1,22 @@
+/*
+ * ¾ËÆÄºª °³¼ö
+ * https://www.acmicpc.net/problem/10808
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj10808 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String S = sc.next();
+		int[] alphabet = new int[26];
+		for (int i = 0; i < S.length(); i++) {
+			alphabet[(int) S.charAt(i) - 'a']++;
+		}
+		for (int i = 0; i < 26; i++) {
+			System.out.print(alphabet[i] + " ");
+		}
+	}
+}
