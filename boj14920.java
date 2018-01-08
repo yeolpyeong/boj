6ab4bfc0bde3eb4fa07b9d64f2d0d0e538510610
김평열 <yeolpyeong@gmail.com>
2018-01-08 17:02:20 +0900

    3n+1 수열

---
+++ boj14920.java
@@ -0,0 +1,25 @@
+/*
+ * 3n+1 ¼ö¿­
+ * https://www.acmicpc.net/problem/14920
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj14920 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int C = sc.nextInt();
+		int n = 1;
+		while (C != 1) {
+			if (C % 2 == 0) {
+				C /= 2;
+			} else {
+				C = 3 * C + 1;
+			}
+			n++;
+		}
+		System.out.print(n);
+	}
+}
