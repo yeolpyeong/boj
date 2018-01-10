8ab9074a2557e67b3405c3ec72e71bf7bef6eac5
김평열 <yeolpyeong@gmail.com>
2018-01-10 15:22:15 +0900

    별찍기 - 21

---
+++ boj10996.java
@@ -0,0 +1,21 @@
+/*
+ * º°Âï±â - 21
+ * https://www.acmicpc.net/problem/10996
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj10996 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i = 1; i <= 2 * N; i++) {
+			for (int j = 1; j <= N; j++) {
+				System.out.print((i + j) % 2 == 0 ? '*' : ' ');
+			}
+			System.out.println();
+		}
+	}
+}
