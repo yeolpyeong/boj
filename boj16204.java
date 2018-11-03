20cf456a3116e509972f3a5f77e987bef93b3524
김평열 <yeolpyeong@gmail.com>
2018-11-03 13:45:53 +0900

    카드 뽑기

---
+++ boj16204.java
@@ -0,0 +1,18 @@
+/*
+ * Ä«µå »Ì±â
+ * https://www.acmicpc.net/problem/16204
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj16204 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int K = sc.nextInt();
+		System.out.print(Math.min(M, K) + Math.min(N - M, N - K));
+	}
+}
