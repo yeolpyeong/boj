9882a25f00530c5ab63b6f11de74725587d327f4
김평열 <yeolpyeong@gmail.com>
2018-11-04 22:48:42 +0900

    막대기

---
+++ boj1094.java
@@ -0,0 +1,16 @@
+/*
+ * ¸·´ë±â
+ * https://www.acmicpc.net/problem/1094
+ */
+
+package boj;
+
+import java.util.Scanner;
+
+public class boj1094 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt();
+		System.out.print(Integer.bitCount(X));
+	}
+}
