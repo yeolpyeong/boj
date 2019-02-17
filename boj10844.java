b387ff9e1cde18bb39f980f7eef6f97a4e16e895
김평열 <yeolpyeong@gmail.com>
2019-02-17 21:14:41 +0900

    쉬운 계단 수

--- boj10844.java
+++ boj10844.java
@@ -3,14 +3,13 @@
  * https://www.acmicpc.net/problem/10844
  */
 
-package boj;
-
 import java.util.Scanner;
 
 public class boj10844 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
+
 		int[][] dp = new int[101][10];
 		for (int i = 1; i <= 9; i++) {
 			dp[1][i] = 1;
@@ -23,6 +22,7 @@ public class boj10844 {
 			}
 			dp[i][9] = dp[i - 1][8];
 		}
+
 		int sum = 0;
 		for (int i = 0; i <= 9; i++) {
 			sum = (sum + dp[N][i]) % mod;
