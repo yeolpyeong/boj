f66edde89e8db06281eaaf72170779e6a557f598
김평열 <yeolpyeong@gmail.com>
2019-02-20 19:12:21 +0900

    그림

--- boj1926.java
+++ boj1926.java
@@ -16,8 +16,8 @@ public class boj1926 {
 		int n = sc.nextInt();
 		int m = sc.nextInt();
 		int[][] map = new int[n][m];
-		for (int i = 0; i < n; ++i) {
-			for (int j = 0; j < m; ++j) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
 				map[i][j] = sc.nextInt();
 			}
 		}
