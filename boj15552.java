08c1732ce5f86ea6d9746e1fac15e6de7e167d8f
김평열 <yeolpyeong@gmail.com>
2019-03-04 19:44:41 +0900

    빠른 A+B

---
+++ boj15552.java
@@ -0,0 +1,27 @@
+/*
+ * ºü¸¥ A+B
+ * https://www.acmicpc.net/problem/15552
+ */
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+class boj15552 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine());
+		while (T-- > 0) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			bw.write(A + B + "\n");
+		}
+		
+		bw.flush();
+	}
+}
