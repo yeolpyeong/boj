8853e4769f4d9a1a8a1b13f222f0fade78fb6aab
김평열 <yeolpyeong@gmail.com>
2019-03-04 19:07:09 +0900

    단어 길이 재기

---
+++ boj2743.java
@@ -0,0 +1,15 @@
+
+/*
+ * 단어 길이 재기
+ * https://www.acmicpc.net/problem/2743
+ */
+
+import java.util.Scanner;
+
+class boj2743 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String word = sc.next();
+		System.out.println(word.length());
+	}
+}
