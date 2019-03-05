de6f2f8f377c8154e913a79da5518d64cd8a0630
김평열 <yeolpyeong@gmail.com>
2019-03-05 21:17:48 +0900

    새로운 시작

--- boj15962.java
+++ boj15962.java
@@ -1,11 +1,11 @@
 /*
- * »õ·Î¿î ½ÃÀÛ
+ * ìƒˆë¡œìš´ ì‹œìž‘
  * https://www.acmicpc.net/problem/15962
  */
 
 
 class boj15962 {
 	public static void main(String[] args) {
-		System.out.print("ÆÄÀÌÆÃ!!");
+		System.out.print("Ã†Ã„Ã€ÃŒÃ†Ãƒ!!");
 	}
 }
