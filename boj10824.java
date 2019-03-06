b01d39a9cca52fdce3e3621caefa47cc7840b584
김평열 <yeolpyeong@gmail.com>
2019-03-06 13:50:45 +0900

    네 수

---
+++ boj10824.java
@@ -0,0 +1,19 @@
+
+/*
+ * ³× ¼ö 
+ * https://www.acmicpc.net/problem/10824
+ */
+
+import java.util.Scanner;
+
+class boj10824 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String A = sc.next();
+		String B = sc.next();
+		String C = sc.next();
+		String D = sc.next();
+
+		System.out.print(Long.parseLong(A + B) + Long.parseLong(C + D));
+	}
+}
