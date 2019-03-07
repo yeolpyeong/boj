dbe6bd140893cf2b4357373144bfd5ecf4fa7d58
김평열 <yeolpyeong@gmail.com>
2019-03-07 17:01:14 +0900

    N-Queen

--- boj9663.java
+++ boj9663.java
@@ -1,47 +1,50 @@
+
 /*
  * N-Queen
  * https://www.acmicpc.net/problem/9663
  */
 
-package boj;
-
 import java.util.Scanner;
 
-public class boj9663 {
-	static int N, res;
-	static int[] cols = new int[16];
+class boj9663 {
+	static int N, count;
+	static int[] cols;
+
+	static boolean promising(int l) {
+		for (int i = 0; i < l; i++) {
+			if (cols[i] == cols[l]) {
+				return false;
+			}
+
+			if (l - i == Math.abs(cols[l] - cols[i])) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	static void dfs(int l) {
+		if (l == N) {
+			count++;
+			return;
+		} else {
+			for (int i = 0; i < N; i++) {
+				cols[l] = i;
+				if (promising(l)) {
+					dfs(l + 1);
+				}
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
-		boj9663(0);
-		System.out.println(res);
-	}
+		cols = new int[N];
 
-	public static boolean boj9663(int l) {
-		if (!promising(l))
-			return false;
-		
-		if (l == N) {
-			res += 1;
-			return false;
-		} else {
-			for (int i = 1; i <= N; i++) {
-				cols[l + 1] = i;
-				if (boj9663(l + 1))
-					return true;
-			}
-		}
-		return false;
-	}
+		dfs(0);
 
-	public static boolean promising(int l) {
-		for (int i = 1; i < l; i++) {
-			if (cols[i] == cols[l])
-				return false;
-			if (l - i == Math.abs(cols[l] - cols[i]))
-				return false;
-		}
-		return true;
+		System.out.print(count);
 	}
 }
