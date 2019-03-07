19850d6fbdd4fd41a846bd295f2f44bf5b1cc4dd
김평열 <yeolpyeong@gmail.com>
2019-03-07 15:27:16 +0900

    연산자 끼워넣기

--- boj14888.java
+++ boj14888.java
@@ -1,63 +1,76 @@
+
 /*
  * 연산자 끼워넣기
  * https://www.acmicpc.net/problem/14888
  */
 
-package boj;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
 
-import java.util.Scanner;
-
-public class boj14888 {
-	static int N, max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+class boj14888 {
+	static int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
 	static int[] A;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		A = new int[N + 1];
-		for (int i = 1; i <= N; i++) {
-			A[i] = sc.nextInt();
-		}
-		int[] OP = new int[5];
-		for (int i = 1; i <= 4; i++) {
-			OP[i] = sc.nextInt();
-		}
-
-		boj14888(1, OP, A[1]);
-		System.out.print(max + "\n" + min);
-	}
-
-	public static void boj14888(int depth, int[] op, int res) {
-		if (depth == N) {
-			max = Math.max(max, res);
-			min = Math.min(min, res);
-		} else {
-			for (int i = 1; i <= 4; i++) {
-				if (op[i] != 0) {
-					int calc = calculation(depth, i, res);
-					op[i]--;
-					boj14888(depth + 1, op, calc);
-					op[i]++;
-				}
+	static void calculate(ArrayList<Integer> operator) {
+		int res = A[0];
+		for (int i = 1; i < A.length; i++) {
+			int op = operator.get(i - 1);
+			if (op == 0) {
+				res += A[i];
+			} else if (op == 1) {
+				res -= A[i];
+			} else if (op == 2) {
+				res *= A[i];
+			} else if (op == 3) {
+				res /= A[i];
 			}
 		}
+
+		max = Math.max(max, res);
+		min = Math.min(min, res);
 	}
 
-	public static int calculation(int depth, int operator, int res) {
-		switch (operator) {
-		case 1:
-			res += A[depth + 1];
-			break;
-		case 2:
-			res -= A[depth + 1];
-			break;
-		case 3:
-			res *= A[depth + 1];
-			break;
-		case 4:
-			res /= A[depth + 1];
-			break;
+	static void swap(ArrayList<Integer> array, int i, int j) {
+		int temp = array.get(i);
+		array.set(i, array.get(j));
+		array.set(j, temp);
+	}
+
+	static void perm(ArrayList<Integer> array, int pivot) {
+		if (pivot == array.size() - 1) {
+			calculate(array);
+			return;
 		}
-		return res;
+
+		for (int i = pivot; i < array.size(); i++) {
+			swap(array, i, pivot);
+			perm(array, pivot + 1);
+			swap(array, i, pivot);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		A = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+		ArrayList<Integer> operator = new ArrayList<>();
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < 4; i++) {
+			int temp = Integer.parseInt(st.nextToken());
+			while (temp-- > 0) {
+				operator.add(i);
+			}
+		}
+
+		perm(operator, 0);
+
+		System.out.print(max + "\n" + min);
 	}
 }
