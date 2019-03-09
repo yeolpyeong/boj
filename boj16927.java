5e2e95f4eb81f738f27534c4af4b11b27863fb0b
김평열 <yeolpyeong@gmail.com>
2019-03-10 00:14:02 +0900

    배열 돌리기 2

--- boj16927.java
+++ boj16927.java
@@ -5,12 +5,11 @@
  */
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class boj16927 {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
