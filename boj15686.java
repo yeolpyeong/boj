ea31b47b777948145ae34dcb1ad9035ee7199049
김평열 <yeolpyeong@gmail.com>
2019-03-09 23:22:23 +0900

    치킨 배달

--- boj15686.java
+++ boj15686.java
@@ -5,7 +5,6 @@
  */
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Stack;
@@ -60,7 +59,7 @@ class boj15686 {
 		dfs(stack, idx + 1);
 	}
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
