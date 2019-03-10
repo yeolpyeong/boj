55d54f4fa28517ded72462462463cf6a78324b2f
김평열 <yeolpyeong@gmail.com>
2019-03-10 14:27:59 +0900

    유진수

---
+++ boj1356.java
@@ -0,0 +1,35 @@
+
+/*
+ * À¯Áø¼ö
+ * https://www.acmicpc.net/problem/1356
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class boj1356 {
+	static int calculate(String n) {
+		int res = 1;
+		for (int i = 0; i < n.length(); i++) {
+			res *= n.charAt(i) - '0';
+		}
+		return res;
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String N = br.readLine();
+		boolean flag = false;
+		for (int i = 1; i < N.length(); i++) {
+			String A = N.substring(0, i);
+			String B = N.substring(i, N.length());
+
+			if (calculate(A) == calculate(B)) {
+				flag = true;
+				break;
+			}
+		}
+
+		System.out.print(flag ? "YES" : "NO");
+	}
+}
