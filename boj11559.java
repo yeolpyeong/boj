dcbef80dc75cf84264bd07008615aa024f669f38
김평열 <yeolpyeong@gmail.com>
2019-03-12 18:21:56 +0900

    Puyo Puyo

--- boj11559.java
+++ boj11559.java
@@ -10,7 +10,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class boj11559 {
-	static final int ROW = 12, COL = 6, BOMB = -1, BLANK = 0;
+	static final int ROW = 12, COL = 6;
 	static final int[] dx = { 1, -1, 0, 0 }, dy = { 0, 0, 1, -1 };
 
 	static class Point {
@@ -22,58 +22,18 @@ class boj11559 {
 		}
 	}
 
-	static int count;
 	static int[][] map;
 
-	static Queue<Point> bfs(boolean[][] visited, int x, int y, int color) {
-		Queue<Point> q1 = new LinkedList<>();
-		Queue<Point> q2 = new LinkedList<>();
-		q1.add(new Point(x, y));
-		q2.add(new Point(x, y));
-		visited[x][y] = true;
-		while (!q1.isEmpty()) {
-			int cx = q1.peek().x;
-			int cy = q1.peek().y;
-			q1.poll();
-
-			for (int d = 0; d < 4; d++) {
-				int nx = cx + dx[d];
-				int ny = cy + dy[d];
-
-				if (nx < 1 || ny < 1 || nx > ROW || ny > COL) {
-					continue;
-				}
-
-				if (map[nx][ny] != color || visited[nx][ny]) {
-					continue;
-				}
-
-				q1.add(new Point(nx, ny));
-				q2.add(new Point(nx, ny));
-				visited[nx][ny] = true;
-			}
-		}
-
-		if (q2.size() < 4) {
-			q2.clear();
-		}
-
-		return q2;
-	}
-
 	static void restore() {
+		int step;
 		for (int j = COL; j >= 1; j--) {
-			int step = 0;
-			for (int i = ROW; i >= 0; i--) {
-				if (map[i][j] == BLANK || (step > 0 && map[i][j] > 0)) {
-					for (int k = step; k >= 1; k--) {
-						map[i + k][j] = i + k > ROW ? 0 : map[i][j];
-					}
-					continue;
-				}
-
-				if (map[i][j] == BOMB) {
+			step = 0;
+			for (int i = ROW; i >= 1; i--) {
+				if (map[i][j] == '.') {
 					step++;
+				} else if (step != 0) {
+					map[i + step][j] = map[i][j];
+					map[i][j] = '.';
 				}
 			}
 		}
@@ -82,21 +42,50 @@ class boj11559 {
 	static boolean bomb() {
 		boolean flag = false;
 		boolean[][] visited = new boolean[ROW + 1][COL + 1];
+		Queue<Point> q1, q2;
 		for (int i = ROW; i >= 1; i--) {
 			for (int j = COL; j >= 1; j--) {
-				if (map[i][j] == BLANK || visited[i][j]) {
+				if (map[i][j] == '.' || visited[i][j]) {
 					continue;
 				}
 
-				Queue<Point> q = bfs(visited, i, j, map[i][j]);
-				if (q.size() >= 4) {
-					flag = true;
-					while (!q.isEmpty()) {
-						int x = q.peek().x;
-						int y = q.peek().y;
-						q.poll();
+				q1 = new LinkedList<>();
+				q2 = new LinkedList<>();
+				q1.add(new Point(i, j));
+				q2.add(new Point(i, j));
+				visited[i][j] = true;
+				while (!q1.isEmpty()) {
+					int cx = q1.peek().x;
+					int cy = q1.peek().y;
+					q1.poll();
 
-						map[x][y] = BOMB;
+					for (int d = 0; d < 4; d++) {
+						int nx = cx + dx[d];
+						int ny = cy + dy[d];
+
+						if (nx < 1 || ny < 1 || nx > ROW || ny > COL) {
+							continue;
+						}
+
+						if (map[nx][ny] != map[cx][cy] || visited[nx][ny]) {
+							continue;
+						}
+
+						q1.add(new Point(nx, ny));
+						q2.add(new Point(nx, ny));
+						visited[nx][ny] = true;
+					}
+				}
+
+				if (q2.size() >= 4) {
+					flag = true;
+
+					while (!q2.isEmpty()) {
+						int x = q2.peek().x;
+						int y = q2.peek().y;
+						q2.poll();
+
+						map[x][y] = '.';
 					}
 				}
 			}
@@ -110,18 +99,15 @@ class boj11559 {
 		for (int i = 1; i <= ROW; i++) {
 			String line = br.readLine();
 			for (int j = 1; j <= COL; j++) {
-				map[i][j] = line.charAt(j - 1) == '.' ? BOMB : line.charAt(j - 1);
+				map[i][j] = line.charAt(j - 1);
 			}
 		}
 
+		int count = 0;
 		restore();
-		while (true) {
-			if (!bomb()) {
-				break;
-			}
-
-			restore();
+		while (bomb()) {
 			count++;
+			restore();
 		}
 
 		System.out.print(count);
