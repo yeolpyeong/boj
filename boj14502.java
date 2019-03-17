e7864b3c7fcd9ea9f73dbc88aa310945b6d8abd9
김평열 <yeolpyeong@gmail.com>
2019-03-17 18:47:42 +0900

    연구소

--- boj14502.java
+++ boj14502.java
@@ -6,7 +6,6 @@
 package boj;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
@@ -17,7 +16,7 @@ public class boj14502 {
 	static int[][] map = new int[9][9];
 	static int[][] visited;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
