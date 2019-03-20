590c6d59c91fc71852bb7c084d344513a558554c
김평열 <yeolpyeong@gmail.com>
2019-03-20 17:36:41 +0900

    파이프 옮기기 2

--- boj17069.java
+++ boj17069.java
@@ -11,8 +11,8 @@ import java.util.StringTokenizer;
 class boj17069 {
 	static long dp(int[][] map) {
 		int N = map.length - 1;
-		long[][][] dp = new long[N + 1][N + 1][3];
-		dp[1][2][0] = 1;
+		long[][][] memoi = new long[N + 1][N + 1][3];
+		memoi[1][2][0] = 1;
 		for (int i = 1; i <= N; i++) {
 			for (int j = 1; j <= N; j++) {
 				if (map[i][j] == 1) {
@@ -20,21 +20,21 @@ class boj17069 {
 				}
 
 				for (int d = 0; d < 3; d++) {
-					if (d != 1 && dp[i][j - 1][d] > 0) {
-						dp[i][j][0] += dp[i][j - 1][d];
+					if (d != 1 && memoi[i][j - 1][d] > 0) {
+						memoi[i][j][0] += memoi[i][j - 1][d];
 					}
 
-					if (d != 0 && dp[i - 1][j][d] > 0) {
-						dp[i][j][1] += dp[i - 1][j][d];
+					if (d != 0 && memoi[i - 1][j][d] > 0) {
+						memoi[i][j][1] += memoi[i - 1][j][d];
 					}
 
-					if (dp[i - 1][j - 1][d] > 0 && map[i][j - 1] == 0 && map[i - 1][j] == 0) {
-						dp[i][j][2] += dp[i - 1][j - 1][d];
+					if (memoi[i - 1][j - 1][d] > 0 && map[i][j - 1] == 0 && map[i - 1][j] == 0) {
+						memoi[i][j][2] += memoi[i - 1][j - 1][d];
 					}
 				}
 			}
 		}
-		return dp[N][N][0] + dp[N][N][1] + dp[N][N][2];
+		return memoi[N][N][0] + memoi[N][N][1] + memoi[N][N][2];
 	}
 
 	public static void main(String[] args) throws Exception {
