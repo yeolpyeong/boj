7d1abb4eb1416a13c7f4165e9df7a68fbeb0a7b8
김평열 <yeolpyeong@gmail.com>
2019-03-21 18:26:06 +0900

    스타트와 링크

--- boj14889.java
+++ boj14889.java
@@ -1,46 +1,18 @@
+
 /*
  * 스타트와 링크
  * https://www.acmicpc.net/problem/14889
  */
 
-package boj;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-import java.util.Scanner;
-
-public class boj14889 {
+class boj14889 {
 	static int N, min = Integer.MAX_VALUE;
 	static int[][] S;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		S = new int[N + 1][N + 1];
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				S[i][j] = sc.nextInt();
-			}
-		}
-
-		boj14889(0, 1, new int[N + 1]);
-		System.out.print(min);
-	}
-
-	public static void boj14889(int n, int m, int[] g) {
-		if (n == N / 2) {
-			min = Math.min(min, scoring(g));
-			return;
-		}
-
-		for (int i = m; i <= N; i++) {
-			if (g[i] == 0) {
-				g[i]++;
-				boj14889(n + 1, i, g);
-				g[i]--;
-			}
-		}
-	}
-
-	public static int scoring(int[] g) {
+	static int scoring(int[] g) {
 		int s1 = 0, s2 = 0;
 		for (int i = 1; i <= N; i++) {
 			for (int j = i + 1; j <= N; j++) {
@@ -52,6 +24,38 @@ public class boj14889 {
 				}
 			}
 		}
-		return (int) Math.abs(s1 - s2);
+		return Math.abs(s1 - s2);
+	}
+
+	static void dfs(int n, int m, int[] g) {
+		if (n == N / 2) {
+			min = Math.min(min, scoring(g));
+			return;
+		}
+
+		for (int i = m; i <= N; i++) {
+			if (g[i] == 0) {
+				g[i]++;
+				dfs(n + 1, i, g);
+				g[i]--;
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		S = new int[N + 1][N + 1];
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= N; j++) {
+				S[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+		dfs(0, 1, new int[N + 1]);
+
+		System.out.print(min);
 	}
 }
