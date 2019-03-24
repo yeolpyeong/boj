e36fe91c77ed08aba4b298e50346af9bfb9ae67a
김평열 <yeolpyeong@gmail.com>
2019-03-24 18:01:27 +0900

    봄버맨 2

---
+++ boj16919.java
@@ -0,0 +1,105 @@
+
+/*
+ * º½¹ö¸Ç 2
+ * https://www.acmicpc.net/problem/16919
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class boj16919 {
+	static final int[] dx = { 1, -1, 0, 0 }, dy = { 0, 0, 1, -1 };
+
+	static class Point {
+		int x, y;
+
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	static char[][] fillWithBombs(char[][] map) {
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[i].length; j++) {
+				map[i][j] = 'O';
+			}
+		}
+		return map;
+	}
+
+	static char[][] explode(char[][] map, Queue<Point> q) {
+		int R = map.length, C = map[0].length;
+		while (!q.isEmpty()) {
+			int cx = q.peek().x;
+			int cy = q.peek().y;
+			q.poll();
+
+			map[cx][cy] = '.';
+			for (int d = 0; d < 4; d++) {
+				int nx = cx + dx[d];
+				int ny = cy + dy[d];
+
+				if (nx < 0 || ny < 0 || nx >= R || ny >= C) {
+					continue;
+				}
+
+				map[nx][ny] = '.';
+			}
+		}
+		return map;
+	}
+
+	static Queue<Point> findBombs(char[][] map) {
+		Queue<Point> q = new LinkedList<>();
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[i].length; j++) {
+				if (map[i][j] == 'O') {
+					q.add(new Point(i, j));
+				}
+			}
+		}
+		return q;
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int R = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		char[][] map = new char[R][C];
+		for (int i = 0; i < R; i++) {
+			String line = br.readLine();
+			for (int j = 0; j < C; j++) {
+				map[i][j] = line.charAt(j);
+			}
+		}
+
+		if (N > 1) {
+			N = (N - 2) % 4 + 2;
+		}
+
+		int second = 0;
+		Queue<Point> q = new LinkedList<>();
+		do {
+			second++;
+			if (second % 2 == 0) {
+				map = fillWithBombs(map);
+			} else {
+				explode(map, q);
+				q = findBombs(map);
+			}
+		} while (second != N);
+
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				System.out.printf("%c", map[i][j]);
+			}
+			System.out.println();
+		}
+	}
+}
