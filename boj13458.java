c79deb3e104c1a8e336d6cac191cb145590f667c
김평열 <yeolpyeong@gmail.com>
2019-03-25 20:00:27 +0900

    시험 감독

--- boj13458.java
+++ boj13458.java
@@ -4,12 +4,11 @@
  */
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class boj13458 {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int[] A = new int[N];
