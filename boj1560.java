5e687ce70fe7523b761226d30dbbdf68740ed8b8
김평열 <yeolpyeong@gmail.com>
2019-03-31 22:40:38 +0900

    비숍

---
+++ boj1560.java
@@ -0,0 +1,18 @@
+
+/*
+ * ºñ¼ó
+ * https://www.acmicpc.net/problem/1560
+ */
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+class boj1560 {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		BigInteger N = sc.nextBigInteger();
+
+		System.out.print(
+				N.compareTo(BigInteger.ONE) == 0 ? 1 : N.subtract(BigInteger.ONE).multiply(BigInteger.valueOf(2)));
+	}
+}
