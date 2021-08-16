781d04ccfa0412eab6db6dc86315345204941058
김평열 <yeolpyeong@gmail.com>
2021-08-16 21:38:21 +0900

    균형잡힌 세상

---
+++ boj4949.java
@@ -0,0 +1,53 @@
+/*
+ * ±ÕÇüÀâÈù ¼¼»ó
+ * https://www.acmicpc.net/problem/4949
+ */
+
+package boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class boj4949 {
+	static void isItBalanced(String string) {
+		Stack<Character> stack = new Stack<>();
+		boolean balanced = true;
+		
+		for (char c : string.toCharArray()) {
+			if (c == '(' || c == '[') {
+				stack.push(c);
+			}
+
+			if (c == ')') {
+				if (stack.isEmpty() || stack.pop() != '(') {
+					balanced = false;
+					break;
+				}
+			} else if (c == ']') {
+				if (stack.isEmpty() || stack.pop() != '[') {
+					balanced = false;
+					break;
+				}
+			}
+		}
+
+		if (!stack.isEmpty()) {
+			balanced = false;
+		}
+
+		System.out.println(balanced ? "yes" : "no");
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+			String string = br.readLine();
+			if (string.equals(".")) {
+				break;
+			}
+			
+			isItBalanced(string);
+		}
+	}
+}
